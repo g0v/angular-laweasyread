af4bb007dc61196f7e1c059a49302eecda921cec
caasi Huang <caasi.igd@gmail.com>
2014-02-07 18:35:48 +0800

    WIP: click to toggle!

--- laweasyread.js
+++ laweasyread.js
@@ -1,9 +1,23 @@
 (function(){
-  angular.module('app.directives', []).directive('lawEasyRead', function(){
+  angular.module('app.directives', []).directive('lawEasyRead', ['$compile'].concat(function($compile){
     return {
       restrict: 'A',
       link: function(scope, elem, attrs){
-        console.log(scope);
+        var el;
+        scope.toggle = function(){
+          return scope.$eval("enabled=" + !scope.enabled);
+        };
+        attrs.$observe('lawEasyRead', function(it){
+          return scope.$eval("enabled=" + it);
+        });
+        (el = elem.clone()).removeAttr('law-easy-read').removeAttr('ng-hide').attr('law-after-easy-read', true).attr('ng-show', 'enabled');
+        return elem.after($compile(el)(scope));
+      }
+    };
+  })).directive('lawAfterEasyRead', function(){
+    return {
+      restrict: 'A',
+      link: function(scope, elem, attrs){
         return LER.parse(elem[0]);
       }
     };
--- src/laweasyread.ls
+++ src/laweasyread.ls
@@ -1,6 +1,18 @@
 angular.module 'app.directives' []
-.directive \lawEasyRead ->
+.directive \lawEasyRead <[$compile]> ++ ($compile) ->
   restrict: \A
   link: (scope, elem, attrs) ->
-    console.log scope
+    scope.toggle = ->
+      scope.$eval "enabled=#{not scope.enabled}"
+    attrs.$observe \lawEasyRead ->
+      scope.$eval "enabled=#it"
+    (el = elem.clone!)
+      .removeAttr \law-easy-read
+      .removeAttr \ng-hide
+      .attr \law-after-easy-read  true
+      .attr \ng-show              \enabled
+    elem.after $compile(el)(scope)
+.directive \lawAfterEasyRead ->
+  restrict: \A
+  link: (scope, elem, attrs) ->
     LER.parse elem.0
